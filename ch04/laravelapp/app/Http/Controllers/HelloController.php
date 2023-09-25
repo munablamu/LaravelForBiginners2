c9617fb83af8696178361f6c92391a07a8dd10be
munablamu <coffee.git@gmail.com>
2023-09-25 15:12:33 +0900

    4.1節まで

---
+++ ch04/laravelapp/app/Http/Controllers/HelloController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class HelloController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        return view('hello.index');
+    }
+
+    public function post(Request $request)
+    {
+        return view('hello.index', ['msg' => $request->msg]);
+    }
+}
---
+++ ch04/laravelapp/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+use App\Http\Middleware\HelloMiddleware;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hello', [HelloController::class, 'index'])->middleware('helo');
+Route::post('hello', [HelloController::class, 'post']);

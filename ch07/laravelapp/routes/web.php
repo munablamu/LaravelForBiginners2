83e9b19a248863d757242ddb401f016db8578285
munablamu <coffee.git@gmail.com>
2023-09-27 12:15:08 +0900

    7.1節まで

---
+++ ch07/laravelapp/routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+use App\Http\Middleware\HelloMiddleware;
+
+use App\Http\Controllers\PersonController;
+use App\Http\Controllers\BoardController;
+use App\Http\Controllers\RestappController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hello', [HelloController::class, 'index']);
+Route::get('hello/show', [HelloController::class, 'show']);
+Route::post('hello', [HelloController::class, 'post']);
+Route::get('hello/add', [HelloController::class, 'add']);
+Route::post('hello/add', [HelloController::class, 'create']);
+Route::get('hello/edit', [HelloController::class, 'edit']);
+Route::post('hello/edit', [HelloController::class, 'update']);
+Route::get('hello/del', [HelloController::class, 'del']);
+Route::post('hello/del', [HelloController::class, 'remove']);
+
+Route::get('person', [PersonController::class, 'index']);
+Route::get('person/find', [PersonController::class, 'find']);
+Route::post('person/find', [PersonController::class, 'search']);
+Route::get('person/add', [PersonController::class, 'add']);
+Route::post('person/add', [PersonController::class, 'create']);
+Route::get('person/edit', [PersonController::class, 'edit']);
+Route::post('person/edit', [PersonController::class, 'update']);
+Route::get('person/del', [PersonController::class, 'delete']);
+Route::post('person/del', [PersonController::class, 'remove']);
+
+Route::get('board', [BoardController::class, 'index']);
+Route::get('board/add', [BoardController::class, 'add']);
+Route::post('board/add', [BoardController::class, 'create']);
+
+Route::resource('rest', RestappController::class);
+Route::get('hello/rest', [HelloController::class, 'rest']);
+
+Route::get('/info', function () {
+    return phpinfo();
+});
